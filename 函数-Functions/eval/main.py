4f630ef0bf231aea604d389ced696352abc7d660
pengchao2022 <pengchao.ma2@outlook.com>
2026-06-15 00:27:55 +0800

    feat: GTP-54273 added the code

---
+++ 函数-Functions/eval/main.py
@@ -0,0 +1,16 @@
+# eval() 将字符串作为表达式进行求值
+
+my_expression = input("Please type a math expession here:\n")
+
+result = eval(my_expression)
+
+print(type(my_expression)) 
+
+print(result)
+
+# 输出结果为
+# Please type a math expession here:
+# 99+98
+# <class 'str'>
+# 197
+# allen@192 eval % 
