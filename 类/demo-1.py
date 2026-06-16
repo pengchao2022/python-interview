9bd9bc62d76f12480f80504906f7701b29d34b6e
pengchao2022 <pengchao.ma2@outlook.com>
2026-06-16 11:30:27 +0800

    feat: GTP-654273 added the class code

---
+++ 类/demo-1.py
@@ -0,0 +1,23 @@
+# define a class and input output changed
+
+class InputOutString(object):
+    
+    # 构造函数
+    def __init__(self):
+        self.s = ""
+
+    # 方法1
+    def getString(self):
+        self.s = input("Please type an English string here: ")
+
+
+    # 方法2 
+    def printString(self):
+        print(f"Your changed string is: {self.s.upper()}")
+
+
+# 创建对象并调用对象的方法    
+strObj = InputOutString()
+strObj.getString()
+strObj.printString()
+
