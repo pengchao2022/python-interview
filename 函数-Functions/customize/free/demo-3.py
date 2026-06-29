810252805f38561b2301b0699d6c1d827eb15295
pengchao2022 <pengchao.ma2@outlook.com>
2026-06-29 19:13:24 +0800

    feat: GTP-54273 added the code

---
+++ 函数-Functions/customize/free/demo-3.py
@@ -0,0 +1,26 @@
+# 编写一个程序，计算以下递推公式的值：
+
+# f(n) = f(n-1) + 100，当 n > 0 时，且 f(0) = 1
+
+# n 由用户在控制台输入（n > 0）。
+
+# 示例：如果输入 n = 5，则程序应输出 500。
+
+def func(n):
+
+    if n == 0:
+        return 1
+    
+    else:
+        return func(n - 1) + 100
+    
+
+def run_app():
+
+    num = int(input("请在此输入一个数字: "))
+    result = func(num)
+
+    print(result)
+
+
+run_app()
