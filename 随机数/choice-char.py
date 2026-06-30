fc57740ab2640ae23f502ccd7992cbf6ea4a8444
pengchao2022 <pengchao.ma2@outlook.com>
2026-06-30 14:18:33 +0800

    feat: GTP-54273 added the code

---
+++ 随机数/choice-char.py
@@ -0,0 +1,8 @@
+import random
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+ran_letter = random.choice(chars)
+
+print(ran_letter)
+
---
+++ 随机数/choice-numbers.py
@@ -0,0 +1,18 @@
+
+import random
+
+
+numbers = '0123456789'
+
+ran_num = random.choice(numbers)
+
+# print(ran_num)
+
+# 随机拿去3个数字
+for _ in range(3):
+
+    print(random.choice(numbers))
+
+
+    
+
