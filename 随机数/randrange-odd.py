fe75369569bc3470dfeef129c9ecd4b4355c25a2
pengchao2022 <pengchao.ma2@outlook.com>
2026-06-30 21:08:14 +0800

    feat: GTP-54273 added the code

---
+++ 随机数/randrange-odd.py
@@ -0,0 +1,12 @@
+# 编写一个程序，随机生成一个包含 5 个奇数的列表，这些偶数的范围在 100 到 200 之间（含 100 和 199）。
+
+# 使用列表推导式
+
+
+
+import random
+
+expect_list = [random.randrange(101, 201, 2) for _ in range(5)]
+
+print(expect_list)
+
