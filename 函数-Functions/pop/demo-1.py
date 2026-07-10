4a47718f10a1b92f7da640b949e6880613ed9d4b
pengchao2022 <pengchao.ma2@outlook.com>
2026-07-10 20:31:22 +0800

    feat: GTP-54273 added the code

--- 函数-Functions/pop/demo-1.py
+++ 函数-Functions/pop/demo-1.py
@@ -12,9 +12,13 @@ num1 = numbers.pop() # 不传入参数时， 返回列表最后的那个元素
 
 print(num1)  # 5    
 
+print(numbers)
+
 num2 = numbers.pop(1)  # 1 是索引 index 从 0 开始
 
 print(num2)     # 2
+
+print(numbers)
 
 
 list1 = ['a', 'b', 'c', 'd']
@@ -23,10 +27,13 @@ char1 = list1.pop()  # 返回列表最后一个元素
 
 print(char1) # d
 
+print(list1)
+
 
 char1 = list1.pop(0)
 
 print(char1) # a
 
+print(list1)
 
 
